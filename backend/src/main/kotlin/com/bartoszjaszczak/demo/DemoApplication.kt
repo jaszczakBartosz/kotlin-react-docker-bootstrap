7664b9cc7d05e995b524fd8900ad739eea655e4d
Bartosz Jaszczak <bartosz.jaszczak@gmail.com>
2025-01-11 13:33:11 +0100

    update of the dependencies

--- backend/src/main/kotlin/com/bartoszjaszczak/demo/DemoApplication.kt
+++ backend/src/main/kotlin/com/bartoszjaszczak/demo/DemoApplication.kt
@@ -2,35 +2,11 @@ package com.bartoszjaszczak.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
-import org.springframework.web.servlet.function.ServerRequest
-import org.springframework.web.servlet.function.ServerResponse
-import org.springframework.web.servlet.function.router
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
-}
-
-@Configuration
-class RouterConfig {
-	@Bean
-	fun userRoutes(handler: UserHandler) = router {
-		"/api/users".nest {
-			GET("", handler::getAllUsers)
-		}
-	}
-}
-
-@Component
-class UserHandler {
-	fun getAllUsers(request: ServerRequest): ServerResponse {
-		// Handler logic
-		return ServerResponse.ok().body("All users")
-	}
 }
 
